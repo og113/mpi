c6fa2ac71bb25defd58e5441d0f5d393affddc5a
og113 <og113@imperial.ac.uk>
2014-07-09 15:46:32 +0100

    fixed startup

--- startup.m
+++ startup.m
@@ -1,7 +1,7 @@
 %adds paths to base
 %adds global parameters
-addpath('/home/og113/Documents/mpi/functions');
-addpath('/home/og113/Documents/mpi/tests');
+addpath('functions');
+addpath('tests');
 
 global d N Na Nb Nc NT L La Lb Lc a b Adim Bdim Cdim Tdim; %defining global variables
 global R X lambda mass v epsilon angle theta;
